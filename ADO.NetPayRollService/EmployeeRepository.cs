6a8c59d4f416f0344abd18cdf1612d719d0ed7ae
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-23 23:03:11 +0530

    adding Pay Roll Service DB Connection

---
+++ ADO.NetPayRollService/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO.NetPayRollService
+{
+    public class EmployeeRepository
+    {
+        public static string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=EmployeePayroll;";
+        //creating object of sqlconnection class and creating connection with database
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
+
+    }
+}
---
+++ ADO.NetPayRollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.NetPayRollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello And Welcome to Pay Roll Service!");
+        }
+    }
+}
